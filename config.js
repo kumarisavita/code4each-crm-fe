ccec29128ed20c92e9e62940fbc4b277b570975f
simran-code4each <simranjit@code4each.com>
2023-11-20 13:10:02 +0530

    set env variables

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const CRM_API_URL = import.meta.env.VITE_CRM_API_URL || 'https://devcrmapi.code4each.comvfdgfdgd';
+const CRM_API_URL = import.meta.env.VITE_CRM_API_URL || 'https://devcrmapi.code4each.com';
 
 export default {
     CRM_API_URL,
--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@ import { createPinia } from 'pinia'
 import { useStore } from './stores/store';
 import App from './App.vue'
 import router from './router'
-import config from '/config';
 
 const app = createApp(App)
 app.config.errorHandler = (err, vm, info) => {
     console.error(err);
 };
-console.log(config.CRM_API_URL, 'oooooooooooo')
+
 app.use(createPinia())
 app.use(useStore());
 app.use(router)
